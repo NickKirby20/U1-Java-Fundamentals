0db459846daf9c2034b9a9f208a7a15506614414
NickKirby20 <kirbynic000@warren.k12.in.us>
2021-08-04 12:52:25 -0400

    Completed L1 ans L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,31 @@
 public class L2 {
 
-    //add the main method header
+    public static void main(String[] args) {
+        double num1 = 3.4;
+        boolean num2 = false;
+        double num3 = 4.5;
+
+
+        String fristName = "George";
+        int age = 16;
+        System.out.println(fristName);
+        System.out.println(age);
+
+
+        String accountName = ("Nick");
+        int accountNumber = 255245876;
+        double balance = 1222222222;
+
+        System.out.println(accountName);
+        System.out.println(accountNumber);
+        System.out.println(balance);
+
+
+
+
+
+
+
+    }
+
 }
