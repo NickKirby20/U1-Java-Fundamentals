98240e1ec3f4a3499304236a0b1847bbbf9197d9
NickKirby20 <kirbynic000@warren.k12.in.us>
2021-09-14 12:54:53 -0400

    1.5 practice problem

--- Finding Averages/src/Averages.java
+++ Finding Averages/src/Averages.java
@@ -5,5 +5,15 @@ public class Averages {
     Example: 12, 13, 14, 15 -> 13.5
      */
 
+    public static void main(String[] args) {
+        int testScore1 = 123246;
+        int testScore2 = 13546311;
+        int testScore3 = 141111;
+        int testScore4 = 1577777;
 
-}
+        int sumTotal = testScore1 + testScore2 + testScore3 + testScore4;
+        double average = sumTotal/4.0;
+
+        System.out.println("The average is " + average);
+      }
+    }
--- Sales Tax/src/SalesTax.java
+++ Sales Tax/src/SalesTax.java
@@ -4,5 +4,32 @@ public class SalesTax {
     Create a program which takes a quantity, price and tax percentage and outputs the total cost with tax.
      */
 
+    public static void main(String[] args) {
+
+        //Declare and initialize variables
+        double price = 9.99;
+        int quantity = 6;
+        double _tax = 7;  //.07
+        final  double TAX = _tax/100.0;
+
+
+        //calculations
+        double subTotal = price * quantity;
+        double taxTotal = subTotal * TAX;
+        double totalCost = subTotal + taxTotal;
+
+        System.out.println("you bought " + quantity + " shirts for " + price + " per shirt.");
+        System.out.println("subtotal: "+ subTotal);
+        System.out.println("tax: " + taxTotal);
+        System.out.println("Total: " + totalCost);
+
+        //DISCOUNT AND INCREASES
+
+        //FORMULA: amount * (1 +/- percentage) = total
+
+    }
+
+
+
 
 }
